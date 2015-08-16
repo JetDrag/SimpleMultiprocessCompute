b01238ddc3465518d00855352f91a93afc287f87
JetDrag <taptube@gmail.com>
2015-08-16 19:18:30 +0800

    Release 1

--- simple_mp.py
+++ simple_mp.py
@@ -3,7 +3,7 @@ __author__ = 'Wang<taptube@gmail.com>'
 
 import multiprocessing,codecs
 
-fhw = codecs.open('result_test','wb','utf8')
+fhw = codecs.open('result','wb','utf8')
 def result_worker(queue,con2):
     tag = 0
     while True:
@@ -16,7 +16,7 @@ def result_worker(queue,con2):
             fhw.writelines(level_path + '\n')
 
 
-class SampleMP(object):
+class SimpleMP(object):
 
     def __init__(self,a_def,task_lst):
         self.def_run = a_def
